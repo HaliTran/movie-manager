cfa8ab96ba2ba877341739604df2a4b6ccdff405
Hali Tran <hali5@pdx.edu>
2023-06-03 19:28:26 -0700

    Modify index route to display movie search form

--- final/app.py
+++ final/app.py
@@ -1,17 +1,18 @@
 """
-A simple web application for listing campus study spaces
+A simple web application for users to search
+and manage their favorite movies and T.V. shows
 
 """
 import flask
 from index import Index
-from entry import Entry
+# Cloud Datastore will be implemented later
+# from entry import Entry
 
 app = flask.Flask(__name__)
 
 app.add_url_rule('/', view_func=Index.as_view('index'), methods=["GET"])
 
-app.add_url_rule('/entry', view_func=Entry.as_view('entry'), methods=["GET", "POST"])
-
+# app.add_url_rule('/entry', view_func=Entry.as_view('entry'), methods=["GET", "POST"])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- final/index.py
+++ final/index.py
@@ -1,9 +1,6 @@
-from flask import render_template
+from flask import render_template, url_for
 from flask.views import MethodView
-import ssmodel
 
 class Index(MethodView):
     def get(self):
-        db = ssmodel.get_model()
-        studySpaces = [dict(building=spaces[0], code=spaces[1], location=spaces[2], rating=spaces[3]) for spaces in db.select()]
-        return render_template("index.html", studySpace = studySpaces)
+        return render_template("index.html")
